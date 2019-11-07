083f6296f0326a3a8614446531f2efe1b5f5b02a
LittleBunnyRabbit <andrazCollege@gmail.com>
2019-11-07 20:46:36 +0100

    Some changes and fixes

--- js/discord/events/onGuildMemberAdd.js
+++ js/discord/events/onGuildMemberAdd.js
@@ -1,13 +1,21 @@
+const {
+    Discord
+} = require("../../Imports.js");
+
 const { def_role } = require("../../../config.json");
 
 module.exports = function(member) {
-    const message = "**HI** :lilpotHype:"
-                  + `\nWelcome to **${member.guild.name}** discord server! We’re happy to have you here. To stay up to date on when lilPotate will go live, follow her on her socials:`
-                  + "\n> **Twitch:** https://www.twitch.tv/lilpotate/"
-                  + "\n> **Twitter:** https://twitter.com/thelilpotate"
-                  + "\n> **Instagram:** https://www.instagram.com/thelilpotate/"
-                  + "\n> **Youtube:** https://www.youtube.com/channel/UCfJWGBY3b-o74Ko8DTidEQg";
+    const socials = "**Twitch:** https://www.twitch.tv/lilpotate/"
+                  + "\n**Twitter:** https://twitter.com/thelilpotate"
+                  + "\n**Instagram:** https://www.instagram.com/thelilpotate/"
+                  + "\n**Youtube:** https://www.youtube.com/channel/UCfJWGBY3b-o74Ko8DTidEQg";
 
-    member.user.send(message);
+    const eWellcome = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setTitle("**HI** <:lilpotHypebot:642086734774927363>")
+        .setDescription(`Welcome to **${member.guild.name}** discord server! We’re happy to have you here. To stay up to date on when lilPotate will go live, follow her on her socials!`)
+        .addField("**SOCIALS**", socials);
+
+    member.user.send(eWellcome);
     member.addRole(def_role);
 };
