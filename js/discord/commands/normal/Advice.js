9850014881e8c11a48abe64584113fd87353195e
LittleBunnyRabbit <andrazCollege@gmail.com>
2019-11-11 19:52:53 +0100

    Added command ADVICE

---
+++ js/discord/commands/normal/Advice.js
@@ -0,0 +1,32 @@
+const {
+    request
+} = require("../../../Imports.js");
+
+module.exports = {
+    name: "ADVICE",
+    description: {
+        "info": "Life advice by lilBot",
+        "uses": {
+            "advice": "gives you adn advice",
+            "advice {mention}": "gives that user an advice"
+        }
+    },
+    execute(client, msg, args) {
+        return sendAdvice(msg);
+    }
+};
+
+async function sendAdvice(msg) {
+    const jAdvice = await JSON.parse(await getJSON("https://api.adviceslip.com/advice"));
+    const mention = msg.mentions.users.first();
+    msg.channel.send(`${mention ? `${mention} ` : ""}${jAdvice.slip.advice}`);
+}
+
+function getJSON(url) {
+    return new Promise(function (resolve, reject) {
+        request(url, function (error, res, body) {
+            if (!error && res.statusCode == 200) resolve(body);
+            else reject(error);
+        });
+    });
+}
