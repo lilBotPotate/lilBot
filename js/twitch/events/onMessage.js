7afeb8c5e29ec69e37c683df379937e025964f2e
LittleBunnyRabbit <andrazCollege@gmail.com>
2019-11-17 19:03:30 +0100

    Removed console.log

--- js/twitch/events/onMessage.js
+++ js/twitch/events/onMessage.js
@@ -1,11 +1,9 @@
 const { prefix } = require("../../../config.json");
 
 module.exports = function(client, channel, tags, message, self, commands) {
-    console.log(`${message} ${message.startsWith(prefix)}`);
     if(!message.startsWith(prefix)) return;
     const args = message.slice(prefix.length).split(/ +/);
     const command = args.shift().toUpperCase();
-    console.log(commands.has(command));
     if(!commands.has(command)) return;
     try {
         `[Twitch]: ${tags.username} executed ${command} ${args}`.sendLog();
