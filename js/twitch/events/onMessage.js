dd2910ef5a4031d00da519a555d355d71573ce13
LittleBunnyRabbit <andrazCollege@gmail.com>
2020-01-13 23:54:02 +0100

    Added icons to twitch chat

--- js/twitch/events/onMessage.js
+++ js/twitch/events/onMessage.js
@@ -1,12 +1,5 @@
 module.exports = function(client, channel, tags, message, self, commands) {
-    if(global.gConfig.twitch_chat && global.gConfig.twitch_chat !== "") {
-        if(tags.username.toUpperCase() !== global.gConfig.twitch_username.toUpperCase()) {
-            const twitchMsg = `**[${tags.username}]:** ${message}`;
-            if(global.gClientDiscord.channels.has(global.gConfig.twitch_chat)) {
-                global.gClientDiscord.channels.get(global.gConfig.twitch_chat).send(twitchMsg);
-            }
-        }
-    }
+    sendToDiscord(tags, message);
 
     if(!message.startsWith(global.gConfig.prefix)) return;
     const args = message.slice(global.gConfig.prefix.length).split(/ +/);
@@ -17,3 +10,19 @@ module.exports = function(client, channel, tags, message, self, commands) {
         return commands.get(command).execute(client, channel, tags, args, self);
     } catch (error) { error.sendLog(); }
 };
+
+function sendToDiscord(tags, message) {
+    if(tags.username.toUpperCase() !== global.gConfig.twitch_username.toUpperCase()) {
+        let badges = "";
+        for(let badege in tags.badges) {
+            const badgeIcon = global.gConfig.twitch_icons[badege];
+            if(badgeIcon) badges += `${badgeIcon} `;
+        }
+
+        const username = tags["display-name"] ? tags["display-name"] : tags.username;
+        const twitchMsg = `${badges !== "" ? badges : ""}**${username}**: ` + "`" + message + "`";
+        if(global.gClientDiscord.channels.has(global.gConfig.twitch_chat)) {
+            global.gClientDiscord.channels.get(global.gConfig.twitch_chat).send(twitchMsg);
+        }
+    }
+}
