b15f8d34d2f0ffe82ab3c480947631e55c43e31a
lilBunnyRabbit <andrazcollege@gmail.com>
2020-03-20 12:23:23 +0100

    Changed exit message

--- src/bots/discord/commands/master/Exit.js
+++ src/bots/discord/commands/master/Exit.js
@@ -11,6 +11,7 @@ module.exports = new Command.Master()
 	  .setCommand(shutDown);
 
 function shutDown(msg) {
-	msg.channel.send("The bot is getting shut down.");
-	process.exit(-1);
+	msg.channel.send("The bot is getting shut down.").then(() => {
+		process.exit(-1);
+	});
 }
