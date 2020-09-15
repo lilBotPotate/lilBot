f6ddf555f1191f5b5d75626b35fbecec1855e8dc
lilBunnyRabbit <andrazcollege@gmail.com>
2020-09-16 00:49:33 +0200

    Revert "On message delete"
    This reverts commit 5b85cd18c95b82403ac194a17b67d1e3eb0cb04f.

--- src/bots/discord/events/onMessageDelete.js
+++
@@ -1,9 +0,0 @@
-const { Discord } = require("../../../imports/Imports");
-
-module.exports = function (msg) {
-  const adminLogChannel = msg.guild.channels.find(
-    (channel) => channel.id == global.gConfig.discord.admin_log_chat
-  );
-  const eDeletedMessage = new Discord.RichEmbed().setColor("#cc0000").setTitle(`Message Deleted`).setDescription(`<@${msg.author.id}>\n${msg.content}`);
-  if (adminLogChannel) adminLogChannel.send(eDeletedMessage);
-};
